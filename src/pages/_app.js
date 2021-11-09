069e7b1e50e30369b8e7863e9c2d5b773c7cec4d
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-11-09 00:32:42 -0300

    adding styled components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    background-color: #2F2E30;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: "#2F2E30",
+  },
+};
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
 
 export default App
