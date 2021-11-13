5a1892d4cc7bf975139de89855e307cd7cd828a1
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-11-13 11:16:53 -0300

    adding searched username to error payload

--- src/pages/api/user/[username].js
+++ src/pages/api/user/[username].js
@@ -21,6 +21,6 @@ export default async function handler(req, res) {
     res.status(200).json(timelineData)
 
   } catch (err) {
-    res.status(404).json({ error: `User ${username} not found` })
+    res.status(404).json({ error: `User ${username} not found`, username })
   }
 }
