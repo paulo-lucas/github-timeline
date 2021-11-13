532b335597ed47765b8a39d4435432a01624739e
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-11-13 11:11:05 -0300

    error message component

--- src/components/Timeline/SearchError.jsx
+++ src/components/Timeline/SearchError.jsx
@@ -1,1 +1,27 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import { AiFillWarning } from "react-icons/ai"
+
+function SearchError({ error: message }) {
+  return <Container>
+    <Icon color="#F1B215" size={32} />
+    <Error>{message}</Error>
+  </Container>
+}
+
+export default SearchError
+
+const Container = styled.div`
+  padding: 3rem;
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+`
+
+const Icon = styled(AiFillWarning)`
+  margin-right: 1rem;
+`
+
+const Error = styled.h2`
+  color: #fff;
+
+`
